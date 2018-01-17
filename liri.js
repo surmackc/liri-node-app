bba8b38e0b164e29e1bedb3bb03352d30b564165
Smack <Smack@Macintosh.local>
2018-01-17 10:49:52 -0500

    setup complete

---
+++ liri.js
@@ -0,0 +1,23 @@
+require("dotenv").config();
+
+
+
+
+
+
+
+
+
+// Access to keys
+var spotify = new Spotify(keys.spotify);
+var client = new Twitter(keys.twitter);
+
+
+
+* `my-tweets`
+
+* `spotify-this-song`
+
+* `movie-this`
+
+* `do-what-it-says`
