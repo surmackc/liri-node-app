75f2a2850fc61c0b8c9e078ce4ff2dc1d8432a0e
Smack <Smack@Macintosh.local>
2018-01-17 19:17:27 -0500

    spotify working

--- liri.js
+++ liri.js
@@ -1,12 +1,8 @@
 require("dotenv").config();
 
-
-
-
-
-
-
-
+var Spotify = require('node-spotify-api');
+var Twitter = require('twitter');
+var keys = require('./keys');
 
 // Access to keys
 var spotify = new Spotify(keys.spotify);
@@ -14,10 +10,48 @@ var client = new Twitter(keys.twitter);
 
 
 
-* `my-tweets`
+if (process.argv[2]==='my-tweets') {
+		myTweets()
+	} else if (process.argv[2]==='spotify-this-song') {
+		findSong();
+	}
 
-* `spotify-this-song`
 
-* `movie-this`
+// * `my-tweets`
+function myTweets(){
+	var params = {screen_name: 'Kevin56614561'};
+client.get('statuses/user_timeline', params, function(error, tweets, response) {
+  if (!error)
+    console.log(JSON.stringify(tweets, null, 2));
+});
+}
 
-* `do-what-it-says`
+// * `spotify-this-song`
+
+function findSong(){
+	spotify
+	.search({ type: 'track', query: process.argv[3], limit: 1 }, function(err, data) {
+  if (err) {
+    return console.log('Error occurred: ' + err);
+  }
+	// console.log(JSON.stringify(data, null, 2));
+	
+	var result = data.tracks.items[0];
+	// console.log(result);
+	 track = {
+	 		artist: result.artists[0].name,
+	 		trackName: result.name,
+	 		preview: result.external_urls.spotify,
+	 		album: result.album.name
+		};
+
+	console.log("Artist: " + track.artist);
+	console.log("Track: " + track.trackName);
+	console.log("Preview: " + track.preview);
+	console.log("Album: " + track.album);
+
+	});
+};
+// * `movie-this`
+
+// * `do-what-it-says`
